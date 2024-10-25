3fcdaccf01c4d5206f332d045db6a16e4fabbcc4
FoxYang12 <foxyang@aaeon.com.tw>
2024-10-25 15:46:35 +0800

    @MODEL:[SRG-IMX8P] @NEW: 1.add aaeon-kirkstone-v01.xml support       2.modify as SRG-IMX8P_BSP_v03 	  3.fix fetcherror @BUG: @EFFECT:

--- scripts-tools/Env/imx-setup-env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-sudo cp file/bblayers.conf ../../../../imx8p_build/conf/bblayers.conf
-sudo cp file/local.conf ../../../../imx8p_build/conf/local.conf
-sudo cp file/imx-setup-release.sh ../../../../imx-setup-release.sh
-
-sync;sync;sync;
